40cf64c28bb4b9327a64510811bec8db3ff809eb
batuhanbaybas <b.baybas@gmail.com>
2023-12-10 19:20:37 +0300

    Remove unused foodSlice reducer

--- src/lib/redux/rootReducer.ts
+++ src/lib/redux/rootReducer.ts
@@ -3,7 +3,6 @@ import {
   categoryApiReducerName
 } from "./slice/categoryApi";
 import { foodApiReducer, foodApiReducerName } from "./slice/foodApi";
-import { foodReducer, foodReducerName } from "./slice/foodSlice";
 
 export const rootReducer = {
   [categoryApiReducerName]: categoryApiReducer,
